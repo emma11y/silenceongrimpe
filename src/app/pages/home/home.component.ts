1f395cc4c483ff2322ffe17d8587a5f2a783c222
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2025-04-16 18:09:17 +0200

    bloc Ils parlent de nous + fix

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CarrouselComponent } from '../../../shared/components/carrousel/carrousel.component';
 import { CollectifComponent } from '../collectif/collectif.component';
+import { TalkAboutUsComponent } from '../talk-about-us/talk-about-us.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CarrouselComponent, CollectifComponent],
+  imports: [CarrouselComponent, CollectifComponent, TalkAboutUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
---
+++ src/app/pages/talk-about-us/talk-about-us.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-talk-about-us',
+  imports: [],
+  templateUrl: './talk-about-us.component.html',
+  styleUrl: './talk-about-us.component.scss',
+})
+export class TalkAboutUsComponent {}
