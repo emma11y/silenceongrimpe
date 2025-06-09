3675ab16a9637d2a2d96f4948c351242964b560b
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2025-06-09 11:07:32 +0200

    add mobile version

--- src/shared/components/header/header.component.ts
+++ src/shared/components/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@node_modules/@angular/common';
 import { RouterLinkActiveDirective } from '@shared/directives/router-link-active.directive';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLinkActiveDirective],
+  imports: [RouterLinkActiveDirective, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  showMenuMobile = false;
+}
