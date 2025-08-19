b73d575ad7da4539172b1b2361a6c29eabe8593c
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2025-08-19 17:22:21 +0200

    init pages + fix css

---
+++ src/app/admin/actualite-form/actualite-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actualite-form',
+  imports: [],
+  templateUrl: './actualite-form.component.html',
+  styleUrl: './actualite-form.component.scss'
+})
+export class ActualiteFormComponent {
+
+}
