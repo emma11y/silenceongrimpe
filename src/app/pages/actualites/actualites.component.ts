043d2fba1a65e2ab5dada0f48b60560a7611b957
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2025-08-28 13:59:28 +0200

    fix css

--- src/app/pages/actualites/actualites.component.ts
+++ src/app/pages/actualites/actualites.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Actualite } from '@shared/models/actualite';
 
 @Component({
   selector: 'app-actualites',
   imports: [],
   templateUrl: './actualites.component.html',
-  styleUrl: './actualites.component.scss'
+  styleUrl: './actualites.component.scss',
 })
 export class ActualitesComponent {
+  private route: ActivatedRoute = inject(ActivatedRoute);
 
+  actualites: Actualite[] = [];
+
+  constructor() {
+    this.actualites = this.route.snapshot.data['actualites'];
+  }
 }
