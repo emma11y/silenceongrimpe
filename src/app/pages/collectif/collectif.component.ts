22c0abee68377fb9d32c130a983140ee6d3cf29a
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2025-09-02 10:04:12 +0200

    fix wording

--- src/app/pages/collectif/collectif.component.ts
+++ src/app/pages/collectif/collectif.component.ts
@@ -4,8 +4,49 @@ import { Component } from '@angular/core';
   selector: 'app-collectif',
   imports: [],
   templateUrl: './collectif.component.html',
-  styleUrl: './collectif.component.scss'
+  styleUrl: './collectif.component.scss',
 })
 export class CollectifComponent {
-
+  membres = [
+    {
+      img: 'Arnaud.jpg',
+      prenom: 'Arnaud',
+      titre: 'Fondateur et coordinateur',
+    },
+    {
+      img: 'Camille.jpg',
+      prenom: 'Camille',
+      titre: 'Infographiste',
+    },
+    {
+      img: 'Celine.jpg',
+      prenom: 'Céline',
+      titre: 'Spécialiste sous-titrage',
+    },
+    {
+      img: 'Emmanuelle.jpg',
+      prenom: 'Emmanuelle',
+      titre: 'Web et spécialiste numérique',
+    },
+    {
+      img: 'Julie.jpg',
+      prenom: 'Julie',
+      titre: 'Spécialiste LSF',
+    },
+    {
+      img: 'Marion.jpg',
+      prenom: 'Marion',
+      titre: 'Communication',
+    },
+    {
+      img: 'Maude.jpg',
+      prenom: 'Maude',
+      titre: 'Spécialiste Montage Ciné',
+    },
+    {
+      img: 'Zehra.jpg',
+      prenom: 'Zehra',
+      titre: 'Détectives',
+    },
+  ];
 }
