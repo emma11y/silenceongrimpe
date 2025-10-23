8161a69764a8ac40da40b83c615820056d512a2e
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2025-10-23 16:20:27 +0200

    fix affichage des actualités

--- src/app/pages/actualites/actualites.component.ts
+++ src/app/pages/actualites/actualites.component.ts
@@ -1,11 +1,13 @@
+import { DatePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { DisplayImageComponent } from '@shared/components/display-image/display-image.component';
 import { Actualite } from '@shared/models/actualite';
+import { orderBy } from 'lodash-es';
 
 @Component({
   selector: 'app-actualites',
-  imports: [DisplayImageComponent, RouterLink],
+  imports: [DisplayImageComponent, RouterLink, DatePipe],
   templateUrl: './actualites.component.html',
   styleUrl: './actualites.component.scss',
 })
@@ -15,6 +17,8 @@ export class ActualitesComponent {
   actualites: Actualite[] = [];
 
   constructor() {
-    this.actualites = this.route.snapshot.data['actualites'];
+    const actualites = this.route.snapshot.data['actualites'];
+
+    this.actualites = orderBy(actualites, (x) => x.datePublication, 'desc');
   }
 }
